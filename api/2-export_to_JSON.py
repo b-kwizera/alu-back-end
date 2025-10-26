3d672b4516e29b61fcd4a9c522206986b8fab088
b-kwizera <b.kwizera@alustudent.com>
2025-10-26 13:23:31 +0200

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-
-''' Export employee TODO list to a JSON file using a REST API.'''
-
+"""
+Export employee TODO list to a JSON file using a REST API.
+"""
 import json
 import requests
 import sys
 
-if __name__ == '__main__':
-    user_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(user_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(user_id)
 
-    user_info = requests.request('GET', user_url).json()
-    todos_info = requests.request('GET', todos_url).json()
+if __name__ == "__main__":
+    emp_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    user_url = f"{base_url}/{emp_id}"
+    todos_url = f"{base_url}/{emp_id}/todos"
 
-    employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj:
-                                 (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
 
-    with open('{}.json'.format(user_id), 'w') as jsonfile:
-        json.dump({user_id: [{"task": task["title"],
-                              "completed": task["completed"],
-                              "username": user_info["username"]}
-                             for task in todos_info]}, jsonfile)
+    emp_username = user.get("username")
+    tasks_list = []
+
+    for task in todos:
+        tasks_list.append({
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": emp_username
+        })
+
+    data = {str(emp_id): tasks_list}
+
+    with open(f"{emp_id}.json", "w") as json_file:
+        json.dump(data, json_file)
