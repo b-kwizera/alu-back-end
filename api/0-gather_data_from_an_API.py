3de1ddc7a386092f112af24e48063414de9a37c9
b-kwizera <b.kwizera@alustudent.com>
2025-10-26 15:05:54 +0200

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,31 +5,16 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        sys.exit(1)
+    employee_id = sys.argv[1]
 
     base_url = "https://jsonplaceholder.typicode.com"
 
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    user_response = requests.get(user_url)
-
-    if user_response.status_code != 200:
-        sys.exit(1)
-
+    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
-    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
-    todos_response = requests.get(todos_url)
-
-    if todos_response.status_code != 200:
-        sys.exit(1)
-
+    todos_response = requests.get("{}/todos".format(base_url),
+                                  params={"userId": employee_id})
     todos_data = todos_response.json()
 
     total_tasks = len(todos_data)
