d261568592c9813aab24dffe22ecd53eed483405
b-kwizera <b.kwizera@alustudent.com>
2025-10-26 13:44:11 +0200

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,27 +1,37 @@
-#!/usr/bin/python3
-"""
-Script that returns information about an employee's TODO list progress.
-"""
+
+"""Script that returns information about an employee's TODO list progress"""
+
 import requests
 import sys
 
 
+"""Script that returns information about an employee's TODO list progres"""
+
 if __name__ == "__main__":
-    emp_id = int(sys.argv[1])
-    base_url = "https://jsonplaceholder.typicode.com/users"
-    user_url = f"{base_url}/{emp_id}"
-    todos_url = f"{base_url}/{emp_id}/todos"
+    user_id = int(sys.argv[1])
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-    user = requests.get(user_url).json()
-    todos = requests.get(todos_url).json()
+    todo_data = requests.get(todos_url).json()
 
-    emp_name = user.get("name")
-    total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed")]
+    employee_name = requests.get(users_url).json()["name"]
 
-    print(
-        f"Employee {emp_name} is done with their tasks"
-        f"({len(done_tasks)}/{total_tasks}):"
-    )
-    for task in done_tasks:
-        print(f"\t {task.get('title')}")
+    total_user_todos = 0
+    completed_todos = 0
+    titles = []
+    for todo in todo_data:
+        # Check if user_id is the same input as parameter
+        if user_id == todo["userId"]:
+            # Get the total number of todos
+            total_user_todos += 1
+            # Get the total number of completed tasks
+            if todo["completed"]:
+                completed_todos += 1
+                # Get the titles of the completed tasks
+                titles.append(todo["title"])
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_todos, total_user_todos))
+
+    for title in titles:
+        print("\t {}".format(title))
